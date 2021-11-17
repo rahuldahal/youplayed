10b288a3178223b5491accf206e8e570c0a7ec25
Rahul Dahal <rdaahal@gmail.com>
2021-11-17 12:36:39 +0545

    refactor: Home screen UI

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { View, Button, Text, Image, StyleSheet, TextInput } from 'react-native';
 import Heading from '../components/Heading';
+import { useAuth } from '../contexts/AuthProvider';
 
 export default function Home() {
+  const [user] = useAuth();
+  console.log(user);
   return (
     <View>
-      <Heading size="1">Authenticated!</Heading>
+      <Image source={{ uri: user.avatar }} />
+      <Heading size="1">Authenticated as {user.fullname}!</Heading>
       <Text>This is the main screen.</Text>
     </View>
   );
