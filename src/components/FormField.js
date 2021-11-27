f9f1433fc7e465821fdedae784e8f4a145fd112b
Rahul Dahal <rdaahal@gmail.com>
2021-11-27 14:22:41 +0545

    refactor: add animations to FormField Label

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Animated, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function FormField({
   label,
@@ -7,15 +7,44 @@ export default function FormField({
   currentState,
   setState,
 }) {
+  const TRANSLATION_START = 32;
+  const translationValue = useState(new Animated.Value(TRANSLATION_START))[0];
+  const ANIMATION_DURATION = 300;
+
+  function animateLabelUp() {
+    Animated.timing(translationValue, {
+      toValue: 8,
+      duration: ANIMATION_DURATION,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function animateLabelDown() {
+    if (currentState !== '') {
+      return;
+    }
+    Animated.timing(translationValue, {
+      toValue: TRANSLATION_START,
+      duration: ANIMATION_DURATION,
+      useNativeDriver: false,
+    }).start();
+  }
+
   return (
     <View style={styles.formField}>
-      <Text style={styles.formLabel}>{label}</Text>
+      <Animated.View style={{ transform: [{ translateY: translationValue }] }}>
+        <Text style={{ color: currentState ? 'red' : 'grey', fontSize: 18 }}>
+          {label}
+        </Text>
+      </Animated.View>
       <TextInput
         style={styles.input}
         onChangeText={setState}
         value={currentState}
         keyboardType={type === 'email' ? 'email-address' : 'default'}
         secureTextEntry={type === 'password'}
+        onFocus={animateLabelUp}
+        onBlur={animateLabelDown}
       />
     </View>
   );
@@ -27,7 +56,6 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   formLabel: {
-    transform: [{ translateY: 32 }],
     color: 'grey',
   },
   input: {
