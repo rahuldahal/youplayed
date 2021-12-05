719b8f7a36e2b90253bb7b007731272948f3aecf
Rahul Dahal <rdaahal@gmail.com>
2021-12-05 18:08:17 +0545

    feat: use VideoOverview Component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,11 +5,12 @@ import Heading from '../components/Heading';
 import { useAuth } from '../contexts/AuthProvider';
 import { YOUTUBE_DATA_API } from '@env';
 import Scrollable from '../components/Scrollable';
+import colors from '../colors';
+import VideoOverview from '../components/VideoOverview';
 
 export default function Home() {
   const [user] = useAuth();
   const [, setIsAuthenticated] = useAuth();
-  const [channelName, onChangeChannelName] = useState('');
 
   async function fetchChannel() {
     const res = await fetch(
@@ -28,24 +29,56 @@ export default function Home() {
     setIsAuthenticated(false);
   }
 
+  const stats = {
+    duration: '1h 45min',
+    likesCount: '3.4K',
+    viewsCount: '1.4M',
+  };
+
   return (
     <Scrollable>
-      <View>
-        <Image
-          style={{ width: 48, height: 48, borderRadius: 48 / 2 }}
-          source={{ uri: user.avatar }}
-        />
-        <Heading size="1">Authenticated as {user.fullname}!</Heading>
-        <Text>This is the main screen.</Text>
-        <TextInput
-          onChangeText={onChangeChannelName}
-          value={channelName}
-          keyboardType="default"
-          placeholder="Channel Name"
-        />
-        <Button title="Search Channel" onPress={fetchChannel} />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Heading size="1">Explore popular videos.</Heading>
+          <Text style={{ color: colors.redishGrey }}>
+            Swipe Right to Watch Later Click to View
+          </Text>
+        </View>
+
+        <View style={styles.videos}>
+          <VideoOverview
+            stats={stats}
+            title="Sample Video Title"
+            channelName="Sample Channel"
+            image="https://picsum.photos/1280/720"
+          />
+          <VideoOverview
+            stats={stats}
+            title="How to this"
+            channelName="How tos"
+            image="https://picsum.photos/1280/720"
+          />
+          <VideoOverview
+            stats={stats}
+            title="Learn x in y hours"
+            channelName="The Learners"
+            image="https://picsum.photos/1280/720"
+          />
+        </View>
         <Button title="Logout" onPress={signOut} />
       </View>
     </Scrollable>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+  },
+  header: {
+    marginTop: 20,
+  },
+  videos: {
+    marginTop: 40,
+  },
+});
