5f1ea92e912e047b434803fbdefa8c6b2ec92f34
Rahul Dahal <rdaahal@gmail.com>
2021-12-05 18:11:22 +0545

    feat: add logout button

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { useAuth } from '../contexts/AuthProvider';
 
 export default function Profile() {
+  const [user] = useAuth();
+  const [, setIsAuthenticated] = useAuth();
+
+  async function signOut() {
+    await auth.signOut();
+    setIsAuthenticated(false);
+  }
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>{user.fullname}</Text>
+      <Button title="Logout" onPress={signOut} />
     </View>
   );
 }
