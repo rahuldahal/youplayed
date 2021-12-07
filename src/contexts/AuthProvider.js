3b96641c1d6533ae526cb29111af209ac25e1121
Rahul Dahal <rdaahal@gmail.com>
2021-12-07 19:02:39 +0545

    refactor: persist user-state

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,12 +1,30 @@
 import React, { createContext, useEffect, useState } from 'react';
+import * as SecureStore from 'expo-secure-store';
 
 const AuthContext = createContext(null);
 
 function AuthProvider({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const user = await SecureStore.getItemAsync('data');
+      const parsedUser = JSON.parse(user);
+      setUser({ data: parsedUser });
+    })();
+  }, []);
+
+  useEffect(() => {
+    user &&
+      (async () => {
+        const [key] = Object.keys(user);
+        const value = user[key];
+        await SecureStore.setItemAsync(key, JSON.stringify(value));
+      })();
+  }, [user]);
 
   return (
-    <AuthContext.Provider value={[isAuthenticated, setIsAuthenticated]}>
+    <AuthContext.Provider value={[(user && user.data) || null, setUser]}>
       {children}
     </AuthContext.Provider>
   );
